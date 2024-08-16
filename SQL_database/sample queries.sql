f7636d7a88366e6cddf361f820989aa438284620
P Anderson <158597021+Paul-Anderson2@users.noreply.github.com>
2024-08-15 19:18:54 -0500

    Add files via upload

---
+++ SQL_database/sample queries.sql
@@ -0,0 +1,17 @@
+SELECT *
+FROM the_used_cars
+
+SELECT *
+FROM simple_cars
+
+SELECT *
+FROM usa_cars
+
+
+SELECT mileage,price
+FROM the_used_cars
+ORDER BY mileage DESC
+
+SELECT transmission, price
+FROM the_used_cars
+ORDER BY transmission
